178a146ae084fdcd88cd3a43452e52d7eaf6eb06
Muhammed K K <ajumell@xeoscript.com>
2026-08-11 23:17:39 +0530

    Implemented authentication token factory for all modules.

---
+++ src/main/java/com/rbl/spa/auth/RBLAuthenticationTokenFactory.java
@@ -0,0 +1,32 @@
+package com.rbl.spa.auth;
+
+import com.xeoscript.unisessions.util.AuthenticationTokenFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class RBLAuthenticationTokenFactory implements AuthenticationTokenFactory {
+
+    @Override
+    public boolean supports(Object principal) {
+        return principal instanceof RBLUser;
+    }
+
+    @Override
+    public Authentication createAuthentication(Object principal, Collection<? extends GrantedAuthority> authorities) {
+        if (principal instanceof RBLUser) {
+            String[] permissions = authorities.stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .toArray(String[]::new);
+
+            RBLUser user = (RBLUser) principal;
+
+            return new RBLAuthenticationToken(user, permissions);
+        }
+
+        return null;
+    }
+}
